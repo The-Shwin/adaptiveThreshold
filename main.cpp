68adf41e1b90f8f5f0ae0b7bec4b05b8f89ff073
Ashwin Bhat <abhat4@jhu.edu>
2018-02-26 20:06:41 -0500

    code for loading and displaying image.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
-    Mat image = imread("maxresdefault.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    
-
+    Mat testImage0 = imread("testImage0.pgm", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat testImage1 = imread("testImage1.pgm", CV_LOAD_IMAGE_GRAYSCALE);
+    namedWindow("Display window 1", WINDOW_AUTOSIZE);
+    imshow("Display window 1", testImage0);
+    waitKey(0);
+    namedWindow("Display window 2", WINDOW_AUTOSIZE);
+    imshow("Display window 2", testImage1);
+    waitKey(0);
 
 
     return 0;
