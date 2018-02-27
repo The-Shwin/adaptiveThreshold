267b6b999da24d947185d6c4dbe932edab98f1af
Ashwin Bhat <abhat4@jhu.edu>
2018-02-26 20:00:45 -0500

    Added cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+    Mat image = imread("maxresdefault.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    
+
+
+
+    return 0;
+}
